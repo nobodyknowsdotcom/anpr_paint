de7e50867548bbad1a1ce2630192182c31ba59d5
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-01-26 01:55:12 +0500

    Fix wrong recognitions via adding BGR sum threshold.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy
 
 def recognize_and_paint(imageCv):
     gray = cv2.cvtColor(imageCv, cv2.COLOR_BGR2GRAY)
+
     bilateral = cv2.bilateralFilter(gray, 11, 17, 17)
     blur = cv2.GaussianBlur(bilateral, (5, 5), 0)
     edged = cv2.Canny(blur, 170, 200)
@@ -16,18 +17,54 @@ def recognize_and_paint(imageCv):
 
     for contour in contours:
         perimeter = cv2.arcLength(contour, True)
-        approximationAccuracy = 0.02 * perimeter
+
+        approximationAccuracy = 0.03 * perimeter
         approximation = cv2.approxPolyDP(contour, approximationAccuracy, True)
 
         if len(approximation) == 4:
             rectangleContours.append(contour)
 
     plateContour = rectangleContours[0]
-    plateBackgroundColor = (255, 255, 255)
+
+    x, y, w, h = cv2.boundingRect(plateContour)
+    plateImage = imageCv[y:y + h, x:x + w]
+
+    plateImageBlur = cv2.GaussianBlur(plateImage, (25, 25), 0)
+
+    plateBackgroundColor = findMostOccurringColor(plateImageBlur)
+
+    if not checkBrighntess(plateBackgroundColor, 10):
+        return imageCv
 
     tempContours3 = cv2.drawContours(imageCv.copy(), [plateContour], -1, plateBackgroundColor, -1)
 
     return tempContours3
+
+
+def findMostOccurringColor(cvImage) -> (int, int, int):
+    width, height, channels = cvImage.shape
+    colorCount = {}
+    for y in range(0, height):
+        for x in range(0, width):
+            BGR = (int(cvImage[x, y, 0]), int(cvImage[x, y, 1]), int(cvImage[x, y, 2]))
+            if BGR in colorCount:
+                colorCount[BGR] += 1
+            else:
+                colorCount[BGR] = 1
+
+    maxCount = 0
+    maxBGR = (0, 0, 0)
+    for BGR in colorCount:
+        count = colorCount[BGR]
+        if count > maxCount:
+            maxCount = count
+            maxBGR = BGR
+
+    return maxBGR
+
+
+def checkBrighntess (BGR, value):
+    return sum(BGR) >= value*3
 
 
 directory = os.fsencode("./samples/")
