18af79e4ee4f3ecad722203e264951a5255c3f41
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-01-25 23:50:01 +0500

    Add directory iterator & result image saver

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 from PIL import Image as imageMain
+import os
 import cv2
 import numpy
 
-imagePath = './samples/9.jpg'
-imagePil = imageMain.open(imagePath)
-imageCv = cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR)
 
-gray = cv2.cvtColor(imageCv, cv2.COLOR_BGR2GRAY)
+def recognize_and_paint(imageCv):
+    gray = cv2.cvtColor(imageCv, cv2.COLOR_BGR2GRAY)
+    bilateral = cv2.bilateralFilter(gray, 11, 17, 17)
+    blur = cv2.GaussianBlur(bilateral, (5, 5), 0)
+    edged = cv2.Canny(blur, 170, 200)
 
-bilateral = cv2.bilateralFilter(gray, 11, 17, 17)
+    contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
+    rectangleContours = []
 
-blur = cv2.GaussianBlur(bilateral, (5, 5), 0)
+    for contour in contours:
+        perimeter = cv2.arcLength(contour, True)
+        approximationAccuracy = 0.02 * perimeter
+        approximation = cv2.approxPolyDP(contour, approximationAccuracy, True)
 
-edged = cv2.Canny(blur, 170, 200)
+        if len(approximation) == 4:
+            rectangleContours.append(contour)
 
-contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
-tempContours1 = cv2.drawContours(imageCv.copy(), contours, -1, (255, 0, 0), 2)
+    plateContour = rectangleContours[0]
+    plateBackgroundColor = (255, 255, 255)
 
-rectangleContours = []
-for contour in contours:
-    perimeter = cv2.arcLength(contour, True)
-    approximationAccuracy = 0.02 * perimeter
-    approximation = cv2.approxPolyDP(contour, approximationAccuracy, True)
-    if len(approximation) == 4:
-        rectangleContours.append(contour)
+    tempContours3 = cv2.drawContours(imageCv.copy(), [plateContour], -1, plateBackgroundColor, -1)
 
-plateContour = rectangleContours[0]
-tempContours2 = cv2.drawContours(imageCv.copy(), [plateContour], -1, (255, 0, 0), 2)
+    return tempContours3
 
-x, y, w, h = cv2.boundingRect(plateContour)
-plateImage = imageCv[y:y + h, x:x + w]
 
-plateImageBlur = cv2.GaussianBlur(plateImage, (25, 25), 0)
+directory = os.fsencode("./samples/")
 
-plateBackgroundColor = (255, 255, 255)
-tempContours3 = cv2.drawContours(imageCv.copy(), [plateContour], -1, plateBackgroundColor, -1)
-cv2.imshow('Result', tempContours3)
-
-cv2.waitKey()
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    imagePil = imageMain.open(directory + file)
+    imageCv = cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR)
+    result = recognize_and_paint(imageCv)
+    cv2.imwrite("./results/" + os.fsdecode(file), result)
