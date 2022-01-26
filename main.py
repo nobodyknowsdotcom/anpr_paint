24995929472dd2675200e9e4e2382b6da43eda2e
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-01-26 23:42:03 +0500

    Add process pool executor

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import numpy as np
 from PIL import Image as imageMain
+import concurrent.futures
 import os
 import cv2
 import numpy
 
 
-def recognize_and_paint(imageCv):
+def recognize_and_paint(file):
+    imagePil = imageMain.open(directory + file)
+    imageCv = cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR)
+
     gray = cv2.cvtColor(imageCv, cv2.COLOR_BGR2GRAY)
 
-    bilateral = cv2.bilateralFilter(gray, 31, 31, 31)
-    blur = cv2.GaussianBlur(bilateral, (5, 5), 0)
-    edged = cv2.Canny(blur, 0, 255, L2gradient=True)
+    bilateral = cv2.bilateralFilter(gray, 11, 17, 17)
+
+    blur = cv2.GaussianBlur(bilateral, (3, 3), 0)
+
+    edged = cv2.Canny(blur, 190, 255, L2gradient=True)
 
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
+
     rectangleContours = []
 
     for contour in contours:
         perimeter = cv2.arcLength(contour, True)
 
-        if perimeter >= 300:
-            approximationAccuracy = 0.0002 * perimeter
-        else:
-            approximationAccuracy = 0.02 * perimeter
+        approximationAccuracy = 0.01718 * perimeter
 
         approximation = cv2.approxPolyDP(contour, approximationAccuracy, True)
 
-        if len(approximation) == 4:
+        x, y, w, h = cv2.boundingRect(contour)
+
+        if (len(approximation) == 4 or len(approximation) == 5) and perimeter < 1000 and 0.7 < w/h < 4.9:
             rectangleContours.append(contour)
 
     if len(rectangleContours) > 0:
@@ -39,13 +45,12 @@ def recognize_and_paint(imageCv):
 
     plateBackgroundColor = findMostOccurringColor(plateImage)
 
-    if not checkBrighntess(plateBackgroundColor, 150):
-        print("small brightness: %.1f" % (np.mean(plateBackgroundColor)))
+    if not checkBrighntess(plateBackgroundColor, 120):
         return imageCv
 
     result = cv2.drawContours(imageCv.copy(), [plateContour], -1, plateBackgroundColor, -1)
 
-    return result
+    cv2.imwrite("./results/" + os.fsdecode(file), result)
 
 
 def findMostOccurringColor(cvImage) -> (int, int, int):
@@ -71,14 +76,13 @@ def findMostOccurringColor(cvImage) -> (int, int, int):
 
 
 def checkBrighntess (BGR, value):
-    return sum(BGR) >= value*3
+    return np.mean(BGR) >= value
 
 
 directory = os.fsencode("./samples/")
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    imagePil = imageMain.open(directory + file)
-    imageCv = cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR)
-    result = recognize_and_paint(imageCv)
-    cv2.imwrite("./results/" + os.fsdecode(file), result)
+with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    futures = []
+    for file in os.listdir(directory):
+        futures.append(executor.submit(recognize_and_paint, file))
+
