55612b346fe6f5717bcd3d9673d77d52a5e603f8
nobodyzhere <myclosestisa@gmail.com>
2022-03-16 01:20:28 +0500

    Improve stability (surround with try/catch) Save output images in .webp

--- main.py
+++ main.py
@@ -7,53 +7,46 @@ import numpy
 
 
 def recognize_and_paint(file):
-    imagePil = imageMain.open(directory + file)
-    imageCv = cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR)
+    try:
+        imagePil = imageMain.open(directory + file)
+        imageCv = cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR)
+        gray = cv2.cvtColor(imageCv, cv2.COLOR_BGR2GRAY)
+        bilateral = cv2.bilateralFilter(gray, 11, 17, 17)
+        blur = cv2.GaussianBlur(bilateral, (3, 3), 0)
+        edged = cv2.Canny(blur, 190, 255, L2gradient=True)
 
-    gray = cv2.cvtColor(imageCv, cv2.COLOR_BGR2GRAY)
+        contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        rectangleContours = []
 
-    bilateral = cv2.bilateralFilter(gray, 11, 17, 17)
+        for contour in contours:
+            perimeter = cv2.arcLength(contour, True)
+            approximationAccuracy = 0.02 * perimeter
+            approximation = cv2.approxPolyDP(contour, approximationAccuracy, True)
+            x, y, w, h = cv2.boundingRect(contour)
 
-    blur = cv2.GaussianBlur(bilateral, (3, 3), 0)
+            if (len(approximation) == 4) and (perimeter < 800):
+                rectangleContours.append(contour)
 
-    edged = cv2.Canny(blur, 190, 255, L2gradient=True)
+        if len(rectangleContours) > 0:
+            plateContour = rectangleContours[0]
+        else:
+            return imageCv
 
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
+        x, y, w, h = cv2.boundingRect(plateContour)
+        plateImage = imageCv[y:y + h, x:x + w]
+        plateBackgroundColor = findMostOccurringColor(plateImage)
 
-    rectangleContours = []
+        if not checkBrighntess(plateBackgroundColor, 100):
+            cv2.imwrite("./results/" + os.fsdecode(file), result, [int(cv2.IMWRITE_WEBP_QUALITY), 75])
 
-    for contour in contours:
-        perimeter = cv2.arcLength(contour, True)
-
-        approximationAccuracy = 0.02 * perimeter
-
-        approximation = cv2.approxPolyDP(contour, approximationAccuracy, True)
-
-        x, y, w, h = cv2.boundingRect(contour)
-
-        if len(approximation) == 4 and perimeter < 1000 and 0.7 < w/h < 4.9:
-            rectangleContours.append(contour)
-
-    if len(rectangleContours) > 0:
-        plateContour = rectangleContours[0]
-    else:
-        return imageCv
-
-    x, y, w, h = cv2.boundingRect(plateContour)
-    plateImage = imageCv[y:y + h, x:x + w]
-
-    plateBackgroundColor = findMostOccurringColor(plateImage)
-
-    if not checkBrighntess(plateBackgroundColor, 100):
-        return imageCv
-
-    result = cv2.drawContours(imageCv.copy(), [plateContour], -1, plateBackgroundColor, -1)
-
-    cv2.imwrite("./results/" + os.fsdecode(file), result)
+        result = cv2.drawContours(imageCv.copy(), [plateContour], -1, plateBackgroundColor, -1)
+        cv2.imwrite("./results/" + os.fsdecode(file), result, [int(cv2.IMWRITE_WEBP_QUALITY), 75])
+    except:
+        cv2.imwrite("./results/" + os.fsdecode(file), cv2.cvtColor(numpy.array(imagePil), cv2.COLOR_RGB2BGR), [int(cv2.IMWRITE_WEBP_QUALITY), 75])
 
 
-def findMostOccurringColor(cvImage) -> (int, int, int):
+def findMostOccurringColor(cvImage):
     width, height, channels = cvImage.shape
     colorCount = {}
     for y in range(0, height):
@@ -81,8 +74,9 @@ def checkBrighntess (BGR, value):
 
 directory = os.fsencode("./samples/")
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
     futures = []
     for file in os.listdir(directory):
         futures.append(executor.submit(recognize_and_paint, file))
+
 
